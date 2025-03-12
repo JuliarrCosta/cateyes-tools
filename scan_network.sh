64d39a6824757f07556f9764b43ad31dfec6ed06
juliarrcosta <juliaraquelrc@outlook.com>
2025-03-11 22:59:23 -0300

    Global Network Scan

---
+++ scan_network.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+
+cat << "EOF"
+
+      _._     _,-'""`-._
+     (,-.`._,'(       |\`-/|
+         `-.-' \ )-`( , o o)
+               `-    \`_`"'- 
+		@cateyes
+		
+Example: ./scan_network.sh 192.168.0.1/24		
+
+EOF
+
+ip_cidr=$1
+ip="${ip_cidr%%/*}"
+mask_bits="${ip_cidr##*/}"
+IFS="." read -r oct1 oct2 oct3 oct4 <<< "$ip"
+
+validate_ip() {
+	 
+	
+	if [[ ! "$ip" =~ ^([0-9]{1,3}\.){3}[0-9]{1,3}$ ]]; then
+		return 1
+	fi
+	
+	
+	for octet in $oct1 $oct2 $oct3 $oct4; do
+		if ((octet < 0 || octet > 254)); then
+			return 1
+		fi
+	done
+	
+	
+	if ! [[ "$mask_bits" =~ ^[0-9]+$ ]] || ((mask_bits < 0 || mask_bits > 31)); then
+		return 1
+	fi
+
+	return 0;
+}
+
+host_number(){
+	local host_bits=$((32 - mask_bits)) 
+	if ((host_bits <= 0)); then
+		return 1;
+	fi
+	local host_number=$(( (2 ** host_bits) - 2 ))
+	echo "$host_number"
+	
+}
+
+ping_network() {
+
+    local hosts=$((2**(32 - mask_bits) - 2))  
+
+    if (( mask_bits >= 24 )); then
+        
+        for (( i=1; i<=254; i++ )); do
+            local target="$oct1.$oct2.$oct3.$i"
+            ping -c 1 $target | grep -i "ttl" | awk '{print $4}' | sed "s/://g" &
+        done
+
+    elif (( mask_bits >= 16 && mask_bits < 24 )); then
+        
+        for (( i=0; i<=255; i++ )); do
+            for (( j=1; j<=254; j++ )); do
+                local target="$oct1.$oct2.$i.$j"
+                ping -c 1 $target | grep -i "ttl" | awk '{print $4}' | sed "s/://g" &
+            done
+        done
+
+    elif (( mask_bits >= 8 && mask_bits < 16 )); then
+        
+        for (( i=0; i<=255; i++ )); do
+            for (( j=0; j<=255; j++ )); do
+                for (( k=1; k<=254; k++ )); do
+                    local target="$oct1.$i.$j.$k"
+                    ping -c 1 $target | grep -i "ttl" | awk '{print $4}' | sed "s/://g" &
+                done
+            done
+        done
+
+    else  # mask_bits < 8
+        
+        for (( i=0; i<=255; i++ )); do
+            for (( j=0; j<=255; j++ )); do
+                for (( k=0; k<=255; k++ )); do
+                    for (( l=1; l<=254; l++ )); do
+                        local target="$i.$j.$k.$l"
+                        ping -c 1 $target | grep -i "ttl" | awk '{print $4}' | sed "s/://g" &
+                    done
+                done
+            done
+        done
+    fi
+
+    wait  
+}
+
+
+main() {
+
+	if validate_ip; then 
+		echo "---------------- HOSTS UP -----------------"  
+		echo "$(ping_network)"
+	else 
+		echo "invalid IP format! Example: 192.168.0.1/24"
+	fi
+
+}
+
+main
+
+
+##Ajustar ping_network, visto que cada octeto o max de address eh 254
